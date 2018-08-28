5fb428008cb8eb28b75ee1f9abd1f488fe78f789
Keith <kperkins411@gmail.com>
2018-08-28 09:52:54 -0400

    init

---
+++ src/deleteme1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : deleteme1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Steal this code!
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
